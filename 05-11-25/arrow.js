16c47a91c08ee4bf56510532e24117aaea5d62a9
Arjun V kumar <arjunvkumar888@gmail.com>
2025-11-05 11:45:50 +0530

    Add files via upload

---
+++ 05-11-25/arrow.js
@@ -0,0 +1,8 @@
+// Without Parameters.
+
+let a = 100;
+let add = () => {
+  console.log("Addition");
+};
+console.log(typeof a); //number
+console.log(typeof add); //function
---
+++ 05-11-25/arrow2.js
@@ -0,0 +1,14 @@
+let getFullName = (fs, md, ln) => {
+  return fs + md + ln;
+};
+let msg = getFullName("Rahul", "Something", "Reddy");
+console.log(msg);
+
+//
+
+let Plus = (one, two, three) => {
+  return one + two + three;
+};
+
+let root = Plus(10, 20, 30);
+console.log(root);
